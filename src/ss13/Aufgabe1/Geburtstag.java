59a5a3668a7a3e19dd24260fa913119e5cd989f6
Hassel Stefan <shutefansan@gmail.com>
2016-06-08 13:18:32 +0200

    Geburtstagsverwaltung SS13 Aufgabe 2

--- src/ss13/Aufgabe1/Geburtstag.java
+++ src/ss13/Aufgabe1/Geburtstag.java
@@ -1,6 +1,6 @@
 package ss13.Aufgabe1;
 
-public class Geburtstag {
+public class Geburtstag implements Comparable<Geburtstag> {
 	private String name;
 	private int tag;
 	private int monat;
@@ -37,10 +37,40 @@ public class Geburtstag {
 	public int getJahr() {
 		return jahr;
 	}
-	
+
 	// b)
 	public String toString() {
 		return "Geburtstag von " + name + " am " + tag + "." + monat + "." + jahr;
 	}
+
+	// c
+	public boolean gleicherTag(Geburtstag g) {
+		if (g.getTag() == this.tag && g.getMonat() == this.monat) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+	
+	public int compareTo(Geburtstag o){
+		int myDay = this.jahr*10000 + this.monat*100+ this.tag;
+		int oDay = o.getJahr()*10000 + o.getMonat()*100 + o.getTag();
+		return myDay - oDay;
+	}
+
+//	@Override
+//	public int compareTo(Geburtstag o) {
+//		if (this.jahr == o.getJahr()) {
+//
+//			if (this.monat == o.getMonat()) {
+//				return this.tag - o.getTag();
+//			} else {
+//				return this.monat - o.getMonat();
+//			}
+//
+//		} else {
+//			return this.jahr - o.getJahr();
+//		}
+//	}
 
 }
